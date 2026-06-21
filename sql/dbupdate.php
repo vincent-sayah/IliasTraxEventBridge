bcdce2a91a57983e6b47a3b087203a5646b43433
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-06-21 11:29:04 +0200

    feat: add v0.4 outbox retry migration

--- sql/dbupdate.php
+++ sql/dbupdate.php
@@ -3,79 +3,20 @@
 /** @var ilDBInterface $ilDB */
 if (!$ilDB->tableExists('evnt_evhk_itxeb_log')) {
     $ilDB->createTable('evnt_evhk_itxeb_log', [
-        'id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true
-        ],
-        'component' => [
-            'type' => 'text',
-            'length' => 255,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'event_name' => [
-            'type' => 'text',
-            'length' => 255,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'user_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'ref_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'obj_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'obj_type' => [
-            'type' => 'text',
-            'length' => 64,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'param_keys' => [
-            'type' => 'clob',
-            'notnull' => false
-        ],
-        'payload_json' => [
-            'type' => 'clob',
-            'notnull' => false
-        ],
-        'request_uri' => [
-            'type' => 'clob',
-            'notnull' => false
-        ],
-        'http_method' => [
-            'type' => 'text',
-            'length' => 16,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'created_at' => [
-            'type' => 'text',
-            'length' => 19,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'created_ts' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ]
+        'id' => ['type' => 'integer', 'length' => 8, 'notnull' => true],
+        'component' => ['type' => 'text', 'length' => 255, 'notnull' => true, 'default' => ''],
+        'event_name' => ['type' => 'text', 'length' => 255, 'notnull' => true, 'default' => ''],
+        'user_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'ref_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'obj_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'obj_type' => ['type' => 'text', 'length' => 64, 'notnull' => true, 'default' => ''],
+        'param_keys' => ['type' => 'clob', 'notnull' => false],
+        'payload_json' => ['type' => 'clob', 'notnull' => false],
+        'request_uri' => ['type' => 'clob', 'notnull' => false],
+        'http_method' => ['type' => 'text', 'length' => 16, 'notnull' => true, 'default' => ''],
+        'created_at' => ['type' => 'text', 'length' => 19, 'notnull' => true, 'default' => ''],
+        'created_ts' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
     ]);
-
     $ilDB->addPrimaryKey('evnt_evhk_itxeb_log', ['id']);
     $ilDB->createSequence('evnt_evhk_itxeb_log');
     $ilDB->addIndex('evnt_evhk_itxeb_log', ['component'], 'i1');
@@ -90,93 +31,22 @@ if (!$ilDB->tableExists('evnt_evhk_itxeb_log')) {
 /** @var ilDBInterface $ilDB */
 if (!$ilDB->tableExists('evnt_evhk_itxeb_out')) {
     $ilDB->createTable('evnt_evhk_itxeb_out', [
-        'id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true
-        ],
-        'event_log_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'statement_uuid' => [
-            'type' => 'text',
-            'length' => 36,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'event_type' => [
-            'type' => 'text',
-            'length' => 64,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'verb_id' => [
-            'type' => 'text',
-            'length' => 255,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'user_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'ref_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'obj_id' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'obj_type' => [
-            'type' => 'text',
-            'length' => 64,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'statement_json' => [
-            'type' => 'clob',
-            'notnull' => false
-        ],
-        'status' => [
-            'type' => 'text',
-            'length' => 32,
-            'notnull' => true,
-            'default' => 'generated'
-        ],
-        'created_at' => [
-            'type' => 'text',
-            'length' => 19,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'created_ts' => [
-            'type' => 'integer',
-            'length' => 8,
-            'notnull' => true,
-            'default' => 0
-        ],
-        'sent_at' => [
-            'type' => 'text',
-            'length' => 19,
-            'notnull' => true,
-            'default' => ''
-        ],
-        'last_error' => [
-            'type' => 'clob',
-            'notnull' => false
-        ]
+        'id' => ['type' => 'integer', 'length' => 8, 'notnull' => true],
+        'event_log_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'statement_uuid' => ['type' => 'text', 'length' => 36, 'notnull' => true, 'default' => ''],
+        'event_type' => ['type' => 'text', 'length' => 64, 'notnull' => true, 'default' => ''],
+        'verb_id' => ['type' => 'text', 'length' => 255, 'notnull' => true, 'default' => ''],
+        'user_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'ref_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'obj_id' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'obj_type' => ['type' => 'text', 'length' => 64, 'notnull' => true, 'default' => ''],
+        'statement_json' => ['type' => 'clob', 'notnull' => false],
+        'status' => ['type' => 'text', 'length' => 32, 'notnull' => true, 'default' => 'generated'],
+        'created_at' => ['type' => 'text', 'length' => 19, 'notnull' => true, 'default' => ''],
+        'created_ts' => ['type' => 'integer', 'length' => 8, 'notnull' => true, 'default' => 0],
+        'sent_at' => ['type' => 'text', 'length' => 19, 'notnull' => true, 'default' => ''],
+        'last_error' => ['type' => 'clob', 'notnull' => false],
     ]);
-
     $ilDB->addPrimaryKey('evnt_evhk_itxeb_out', ['id']);
     $ilDB->createSequence('evnt_evhk_itxeb_out');
     $ilDB->addIndex('evnt_evhk_itxeb_out', ['event_log_id'], 'i1');
@@ -184,5 +54,27 @@ if (!$ilDB->tableExists('evnt_evhk_itxeb_out')) {
     $ilDB->addIndex('evnt_evhk_itxeb_out', ['event_type'], 'i3');
     $ilDB->addIndex('evnt_evhk_itxeb_out', ['status'], 'i4');
     $ilDB->addIndex('evnt_evhk_itxeb_out', ['created_ts'], 'i5');
+}
+?>
+<#3>
+<?php
+/** @var ilDBInterface $ilDB */
+if ($ilDB->tableExists('evnt_evhk_itxeb_out')) {
+    if (!$ilDB->tableColumnExists('evnt_evhk_itxeb_out', 'retry_count')) {
+        $ilDB->addTableColumn('evnt_evhk_itxeb_out', 'retry_count', [
+            'type' => 'integer', 'length' => 4, 'notnull' => true, 'default' => 0,
+        ]);
+    }
+    if (!$ilDB->tableColumnExists('evnt_evhk_itxeb_out', 'max_retry')) {
+        $ilDB->addTableColumn('evnt_evhk_itxeb_out', 'max_retry', [
+            'type' => 'integer', 'length' => 4, 'notnull' => true, 'default' => 5,
+        ]);
+    }
+    if (!$ilDB->tableColumnExists('evnt_evhk_itxeb_out', 'last_attempt_at')) {
+        $ilDB->addTableColumn('evnt_evhk_itxeb_out', 'last_attempt_at', [
+            'type' => 'text', 'length' => 19, 'notnull' => true, 'default' => '',
+        ]);
+    }
+    $ilDB->manipulate('UPDATE evnt_evhk_itxeb_out SET max_retry = 5 WHERE max_retry = 0');
 }
 ?>
