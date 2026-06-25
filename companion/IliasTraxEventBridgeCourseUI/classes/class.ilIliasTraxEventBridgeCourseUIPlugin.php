0762c0d7de43c1ae1c5dc481cfe6265434ea7cff
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-06-25 18:36:45 +0200

    feat: add course ui companion plugin class

---
+++ companion/IliasTraxEventBridgeCourseUI/classes/class.ilIliasTraxEventBridgeCourseUIPlugin.php
@@ -0,0 +1,30 @@
+<?php
+
+/**
+ * UIHook companion plugin for IliasTraxEventBridge.
+ *
+ * This plugin is intentionally lightweight. It exposes course-level UI entry
+ * points and delegates storage/filtering to the main EventHook plugin.
+ */
+class ilIliasTraxEventBridgeCourseUIPlugin extends ilUserInterfaceHookPlugin
+{
+    public const PLUGIN_NAME = 'IliasTraxEventBridgeCourseUI';
+    public const MAIN_PLUGIN_NAME = 'IliasTraxEventBridge';
+
+    public function getPluginName(): string
+    {
+        return self::PLUGIN_NAME;
+    }
+
+    public function isMainPluginAvailable(): bool
+    {
+        return is_dir($this->getMainPluginPath());
+    }
+
+    public function getMainPluginPath(): string
+    {
+        return dirname(__DIR__, 3)
+            . '/EventHandling/EventHook/'
+            . self::MAIN_PLUGIN_NAME;
+    }
+}
