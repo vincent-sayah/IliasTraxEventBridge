78ac5d7f3c2c42735f642a3ece6cba898e748d6d
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-06-25 18:36:31 +0200

    feat: add course ui companion plugin skeleton

---
+++ companion/IliasTraxEventBridgeCourseUI/plugin.php
@@ -0,0 +1,8 @@
+<?php
+
+$id = "itxebcui";
+$version = "0.1.0";
+$ilias_min_version = "10.0.0";
+$ilias_max_version = "10.999.999";
+$responsible = "TRAX / ILIAS integration";
+$responsible_mail = "noreply@localhost";
