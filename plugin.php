9b06d8af602f1ebf1cfb07cf9a4e99d7af33f490
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-07-02 20:52:32 +0200

    Start v0.13.0 development

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 $id = 'itxeb';
-$version = '0.12.0';
+$version = '0.13.0-dev';
 $ilias_min_version = '10.0.0';
 $ilias_max_version = '10.999.999';
 $responsible = 'TRAX / ILIAS integration';
