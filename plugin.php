18b971942ea8fe5e23b8a594f84f55c422a30cc8
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-07-03 21:13:34 +0200

    Release V0.12.1

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 $id = 'itxeb';
-$version = '0.12.1-dev';
+$version = '0.12.1';
 $ilias_min_version = '10.0.0';
 $ilias_max_version = '10.999.999';
 $responsible = 'TRAX / ILIAS integration';
