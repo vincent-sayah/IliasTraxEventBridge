58795b3e2ffa2ad9c57f044a265a718736533cff
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-07-03 21:06:09 +0200

    Retarget consolidation work to V0.12.1

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 $id = 'itxeb';
-$version = '0.13.0-dev';
+$version = '0.12.1-dev';
 $ilias_min_version = '10.0.0';
 $ilias_max_version = '10.999.999';
 $responsible = 'TRAX / ILIAS integration';
