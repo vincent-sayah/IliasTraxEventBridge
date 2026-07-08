6db5bef4da514c5b7ea2ead7167abc7dc525a149
Vince <61915948+vincent-sayah@users.noreply.github.com>
2026-07-08 18:05:47 +0200

    Bump plugin to V0.15.2-dev

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 $id = 'itxeb';
-$version = '0.15.0-dev';
+$version = '0.15.2-dev';
 $ilias_min_version = '10.0.0';
 $ilias_max_version = '10.999.999';
 $responsible = 'TRAX / ILIAS integration';
