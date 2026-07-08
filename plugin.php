69d766574c899da48241e961f504b2d63501c706
ChatGPT Artifact Builder <artifact-builder@example.local>
2026-07-08 21:10:50 +0200

    V0.19.1 use checkbox AI history comparison

--- plugin.php
+++ plugin.php
@@ -1,7 +1,7 @@
 <?php
 
 $id = 'itxeb';
-$version = '0.18.0-dev';
+$version = '0.19.1-dev';
 $ilias_min_version = '10.0.0';
 $ilias_max_version = '10.999.999';
 $responsible = 'TRAX / ILIAS integration';
